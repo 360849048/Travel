0d03c7bdd3e0008d7d630bf471d934889f548160
13555 <13555/HAITIAN>
2018-12-18 08:16:32 +0800

    使用了递归组件

--- src/page/detail/Detail.vue
+++ src/page/detail/Detail.vue
@@ -2,19 +2,34 @@
   <div class="detail">
     <detail-banner />
     <detail-header />
-    hello
+    <detail-list :list="list" />
   </div>
 </template>
 
 <script>
   import DetailBanner from '@/page/detail/components/Banner'
   import DetailHeader from '@/page/detail/components/Header'
+  import DetailList from '@/page/detail/components/List'
 
   export default {
     name: "detail",
     components: {
       DetailBanner,
-      DetailHeader
+      DetailHeader,
+      DetailList
+    },
+    data () {
+      return {
+        list: [
+          {title: '成人票', children: [
+              {title: '场馆一'},
+              {title: '场馆二'}
+            ]},
+          {title: '学生票'},
+          {title: '儿童票'},
+          {title: '特惠票'}
+        ]
+      }
     }
   }
 </script>
---
+++ src/page/detail/components/List.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="detail-list-wrapper">
+    <div class="item-list" v-for="(item, index) in list" :key="index">
+      <div class="item-title">
+        <span class="title-icon"></span>
+        {{item.title}}
+      </div>
+      <div class="item-children" v-if="item.children">
+        <detail-list :list="item.children" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DetailList",
+    props: {
+      list: Array
+    }
+  }
+</script>
+
+<style scoped>
+ .title-icon {
+   display: inline-block;
+   position: relative;
+   top: .08rem;
+   left: .2rem;
+   width: .36rem;
+   height: .36rem;
+   background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+   margin-right: .1rem;
+   background-size: .4rem 3rem;
+ }
+  .item-title {
+    line-height: .8rem;
+    font-size: .32rem;
+    padding: 0 .1rem;
+    border-bottom: 1px solid #ccc;
+  }
+  .item-children {
+    text-indent: .4rem;
+  }
+</style>
